dff8e30f6d0f4da8306a19a31d0d6a88961be59f
Irmeen <irmeenhusain001@gmail.com>
2024-08-22 03:58:25 +0530

    Authentication using google and setting the database

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./styles.css";
+import {auth} from "../../firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { signOut } from "firebase/auth";
+
+
 
 function Header() {
-  function logoutFnc(){
-    alert("logout!!");
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(user){
+      navigate("/dashboard");
+    }
+  }, [user,loading]);
+
+function logoutFnc(){
+  try{
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      toast.success("Logged Out Successfully!");
+      navigate("/");
+      
+    }).catch((error) => {
+      // An error happened.
+      toast.error(error.message);
+    });
+  }catch(e){
+    toast.error(e.message);
   }
+  
+}
+
   return (
     <div className='navbar'>
       <p className='logo'>Financium</p>
-      <p className='logo link' onClick={logoutFnc}>Logout</p>
+      {user &&  (
+        <p className='logo link' onClick={logoutFnc}>
+          Logout
+        </p>
+      )}
     </div>
   )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Header from "../components/Header";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div> 
+      <Header /> 
+    </div>
   )
 }
 
